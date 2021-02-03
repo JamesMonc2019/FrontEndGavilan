5615969658bc3ff3072718364982ad5fb1224aa7
JamesMonc <jm36864@gmail.com>
2021-02-03 17:20:41 -0600

    hasta el video 128 segundo commit

---
+++ src/app/seguridad/seguridad.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { credencialesUsuario, respuestaAutenticacion } from './seguridad';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeguridadService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  apiURL = environment.apiURL + 'cuentas'
+  estaLogueado(): boolean{
+    return false;
+  }
+
+  obtenerRol(): string {
+    return 'admin';
+  }
+
+  registrar(credenciales: credencialesUsuario):Observable<respuestaAutenticacion>{
+    return this.httpClient.post<respuestaAutenticacion>(this.apiURL + '/crear', credenciales);
+  }
+}
